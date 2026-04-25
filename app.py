cacd178b2030a3be51b54e359f9aba512cd636cc
dvrallard-cmyk <dvrallard@gmail.com>
2026-04-25 13:32:27 -0500

    Update app.py
    introducir codigo /app_v0.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mi App", page_icon="👋")
+
+st.title("Hola 👋")
+st.subheader("Bienvenido a tu nueva App")
+st.write("Esta es una prueba básica de Streamlit.")
 
